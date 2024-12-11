d895aeb471442eb5a25ccd252a845db90aaa90b0
adersonbertim <adersonbertim@gmail.com>
2024-12-11 15:48:26 -0300

    add: printf function

--- Estrutura de dados/structs/exercicios de fixação/ex2.c
+++ Estrutura de dados/structs/exercicios de fixação/ex2.c
@@ -21,5 +21,9 @@ int main() {
   triangulo.pt2.y = 0;
   triangulo.pt3.y = 1;
 
+  printf("Ponto 1: (%d, %d)\n", triangulo.pt1.x, triangulo.pt1.y);
+  printf("Ponto 2: (%d, %d)\n", triangulo.pt2.x, triangulo.pt2.y);
+  printf("Ponto 3: (%d, %d)\n", triangulo.pt3.x, triangulo.pt3.y);
+
   return 0;
 }
